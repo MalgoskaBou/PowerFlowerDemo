7a2ea72f57bfb3e8a9251e69998cb3357ed76b23
MalgoskaBou <ghouga.m@gmail.com>
2020-02-26 22:27:10 +0100

    Updates

---
+++ precache-manifest.c3bb121ae64573e6bc974bd4883b07f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a99e87b3f0273347ede4a83b96f661",
+    "url": "/PowerFlowerDemo/index.html"
+  },
+  {
+    "revision": "899b1e27bd55e62cd462",
+    "url": "/PowerFlowerDemo/static/css/main.dcdaa742.chunk.css"
+  },
+  {
+    "revision": "8684cea244c200b2d357",
+    "url": "/PowerFlowerDemo/static/js/2.9872f2a6.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/PowerFlowerDemo/static/js/2.9872f2a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "899b1e27bd55e62cd462",
+    "url": "/PowerFlowerDemo/static/js/main.854b7357.chunk.js"
+  },
+  {
+    "revision": "1fec583f53668de9fa21",
+    "url": "/PowerFlowerDemo/static/js/runtime-main.f9b4a150.js"
+  },
+  {
+    "revision": "fa50e726db409fbc9e7472c2038e6eaa",
+    "url": "/PowerFlowerDemo/static/media/email.fa50e726.svg"
+  },
+  {
+    "revision": "c77bfdea5debc5779e405a76e8b9f224",
+    "url": "/PowerFlowerDemo/static/media/icon_mail.c77bfdea.svg"
+  },
+  {
+    "revision": "38816da4dccb8304351ebedb62127729",
+    "url": "/PowerFlowerDemo/static/media/icon_password.38816da4.svg"
+  },
+  {
+    "revision": "c7a2568e14fa0021a93a8d251b39553c",
+    "url": "/PowerFlowerDemo/static/media/password.c7a2568e.svg"
+  }
+]);
